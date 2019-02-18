5e60076ef20754b2c2442c868dad870c7259898c
kondo takeshi <chaspy@users.noreply.github.com>
2019-02-19 02:45:44 +0900

    Create table

---
+++ 1_postgresql/2_createtable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE countries (
+  country_code char(2) PRIMARY KEY,
+  country_name text UNIQUE
+);
+
+INSERT INTO countries (country_code, country_name)
+VALUES ('us','United States'), ('mx','Mexico'), ('au','Australia'),
+       ('gb','United Kingdom'), ('de','Germany'), ('ll','Loompaland');
